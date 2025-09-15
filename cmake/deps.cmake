736f55fd86ccc4a606eb2c8da6121180459fdcdd
dr3f <dr3f.sounds@gmail.com>
2025-09-15 22:15:18 +0000

    Cleanup

--- cmake/deps.cmake
+++ cmake/deps.cmake
@@ -4,6 +4,13 @@ find_package(ALSA REQUIRED)
 find_package(Boost REQUIRED)
 
 FetchContent_Declare(
+    readerwriterqueue
+    GIT_REPOSITORY https://github.com/cameron314/readerwriterqueue.git
+    GIT_TAG        v1.0.7
+)
+FetchContent_MakeAvailable(readerwriterqueue)
+
+FetchContent_Declare(
     libremidi
     GIT_REPOSITORY https://github.com/celtera/libremidi
     GIT_TAG        v5.3.1
